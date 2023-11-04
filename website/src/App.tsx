d4c456f71b90f607bb0334a8cd462b05b6b4da18
davidcraccer <dsvoboda25@gmail.com>
2023-11-04 16:28:57 +0100

    layouts + config + navbar
    Co-authored-by: RealChoki <RealChoki@users.noreply.github.com>

---
+++ website/src/App.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <div className="test"></div>
+      <div className="test2"></div>
+    </div>
+  );
+}
+
+export default App;

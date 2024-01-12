41541255f121ce5046cb01e2afd47988c05a568f
davidcraccer <dsvoboda25@gmail.com>
2024-01-12 18:15:32 +0100

    update notificationsdata

--- website/src/components/NavBar/Notification/notifcationsData.ts
+++ website/src/components/NavBar/Notification/notifcationsData.ts
@@ -2,49 +2,61 @@ export const notifications = [
   {
     sender: "System",
     receiver: ["Mitarbeiter", "Aushilfskraft"],
+    thema: "Krankmeldung",
     message:
       "Sie haben erfolgreich Ihre Krankmeldung gemeldet. Eine E-Mail wurde automatisch generiert und an Ihre Krankenversicherung gesendet zwecks weiterer Bearbeitung.",
   },
   {
     sender: "System",
     receiver: ["Mitarbeiter", "Aushilfskraft"],
+    thema: "UrlaubsantragGenehmigung",
     message: "Ihr Urlaubsantrag wurde erfolgreich genehmigt.",
   },
   {
     sender: "System",
     receiver: ["Mitarbeiter", "Aushilfskraft"],
+    thema: "ÜberstundenGenehmigung",
     message: "Ihre Überstundenanfrage wurde erfolgreich genehmigt.",
   },
   {
     sender: "System",
     receiver: ["Mitarbeiter", "Aushilfskraft"],
-    message: "Ihre Überstundenanfrage wurde leider abgelehnt. Überprüfen Sie die App für weitere Details und überlegen Sie eine erneute Einreichung.",
+    thema: "ÜberstundenAblehnung",
+    message:
+      "Ihre Überstundenanfrage wurde leider abgelehnt. Überprüfen Sie die App für weitere Details und überlegen Sie eine erneute Einreichung.",
   },
   {
     sender: "System",
     receiver: ["Mitarbeiter", "Aushilfskraft"],
-    message: "Ihr Urlaubsantrag wurde leider abgelehnt. Überprüfen Sie die App für weitere Details und überlegen Sie eine erneute Einreichung.",
+    thema: "UrlaubsantragAblehnung",
+    message:
+      "Ihr Urlaubsantrag wurde leider abgelehnt. Überprüfen Sie die App für weitere Details und überlegen Sie eine erneute Einreichung.",
   },
   {
     sender: "Mitarbeiter",
     receiver: ["Vorgesetzte", "Niederlassungsleiter", "Personalabteilung"],
-    message: "hat sich krankgemeldet. Bitte beachten Sie dies für HR-Aufzeichnungen und Abrechnungszwecke.",
+    thema: "Krankmeldung",
+    message:
+      "hat sich krankgemeldet. Bitte beachten Sie dies für HR-Aufzeichnungen und Abrechnungszwecke.",
   },
   {
     sender: "Mitarbeiter",
     receiver: ["Vorgesetzte", "Niederlassungsleiter", "Personalabteilung"],
-    message: "hat einen Urlaubsantrag gestellt. Bitte überprüfen Sie und nehmen Sie entsprechende Maßnahmen.",
+    thema: "Urlaubsanfrage",
+    message:
+      "hat einen Urlaubsantrag gestellt. Bitte überprüfen Sie und nehmen Sie entsprechende Maßnahmen.",
   },
   {
     sender: "Mitarbeiter",
     receiver: ["Vorgesetzte", "Niederlassungsleiter", "Personalabteilung"],
-    message: "hat seine Arbeitszeit erfasst. Überprüfen Sie die Informationen für Abrechnungszwecke.",
+    thema: "Arbeitszeiterfassung",
+    message:
+      "hat seine Arbeitszeit erfasst. Überprüfen Sie die Informationen für Abrechnungszwecke.",
   },
   {
     sender: "Mitarbeiter",
     receiver: ["Vorgesetzte", "Niederlassungsleiter", "Personalabteilung"],
-    thema: "ÜberstundenAnfrage",
+    thema: "Überstundenanfrage",
     message: "hat Überstunden angehäuft und beantragt deren Genehmigung.",
-  }
-  
+  },
 ];
